2bc9912d66fc51c5360d7c2730b4f5de72f1e574
Kevin Umbach <kumbach@gmail.com>
2014-05-22 13:38:44 -0600

    added comments to identify some of the grammer that describes the structure of the classes

--- Hello World/HelloOOP/Greeter.cs
+++ Hello World/HelloOOP/Greeter.cs
@@ -6,55 +6,66 @@ using System.Threading.Tasks;
 
 namespace HelloOOP
 {
-    public class Greeter
+  // this is a class named Greeter. It is public, which means any other class can create a new instance of it.
+  public class Greeter
+  {
+    #region Fields and Properties
+    // traditional Field + Property combination
+
+    // this is a field
+    private string _GreetingMessage;
+
+    // this is a property
+    public string GreetingMessage
     {
-        #region Fields and Properties
-        // traditional Field + Property combination
-        private string _GreetingMessage;
-        public string GreetingMessage
-        {
-            get
-            {
-                return _GreetingMessage;
-            }
-            private set
-            {
-                _GreetingMessage = value;
-            }
-        }
-
-        // Auto-implemented Property (field as a "backing store" is "auto-generated")
-        public string FarewellMessage { get; private set; }
-        #endregion
-
-        #region Constructors
-        public Greeter(string greeting, string farewell)
-        {
-            GreetingMessage = greeting;
-            FarewellMessage = farewell;
-        }
-        #endregion
-
-        #region Methods
-        public string SayGreeting(string name)
-        {
-            return name + ", " + GreetingMessage;
-        }
-
-        public string SayGreeting()
-        {
-            return GreetingMessage;
-        }
-
-        public string SayGoodbye(string name)
-        {
-            return name + " - " + FarewellMessage;
-        }
-
-        public string SayGoodbye()
-        {
-            return FarewellMessage;
-        }
-        #endregion
+      get
+      {
+        return _GreetingMessage;
+      }
+      private set
+      {
+        _GreetingMessage = value;
+      }
     }
+
+    // Auto-implemented Property (field as a "backing store" is "auto-generated")
+    // this is a property that has thegetter and setter implemented automatically
+    public string FarewellMessage { get; private set; }
+    #endregion
+
+    #region Constructors
+    // this is a method
+    public Greeter(string greeting, string farewell)
+    {
+      GreetingMessage = greeting;
+      FarewellMessage = farewell;
+    }
+    #endregion
+
+    #region Methods
+    // this is a method
+    public string SayGreeting(string name)
+    {
+      return name + ", " + GreetingMessage;
+    }
+
+    // this is a method
+    public string SayGreeting()
+    {
+      return GreetingMessage;
+    }
+
+    // this is a method
+    public string SayGoodbye(string name)
+    {
+      return name + " - " + FarewellMessage;
+    }
+
+    // this is a method
+    public string SayGoodbye()
+    {
+      return FarewellMessage;
+    }
+    #endregion
+  }
 }
--- Hello World/HelloOOP/Program.cs
+++ Hello World/HelloOOP/Program.cs
@@ -6,32 +6,35 @@ using System.Threading.Tasks;
 
 namespace HelloOOP
 {
-    class Program
+  // this is a class named Program. because there is no accessModifier specified, it's access defaults to "internal".
+  class Program
+  {
+    // this is a static method and happens to be the method that is called to start the program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // Declare two variables (object references)
-            Greeter walmartEmployee, klingonWarrior;
+      // Declare two variables (object references)
+      Greeter walmartEmployee, klingonWarrior;
 
-            // Instantiate (create) the objects
-            walmartEmployee = new Greeter("Welcome to Walmart!",
-                                          "Thank you for shopping at Walmart!!");
-            klingonWarrior = new Greeter("nuqneH! yI'el!",
-                                         "Qapla'!");
+      // Instantiate (create) the objects
+      walmartEmployee = new Greeter("Welcome to Walmart!",
+                                    "Thank you for shopping at Walmart!!");
+      klingonWarrior = new Greeter("nuqneH! yI'el!",
+                                   "Qapla'!");
 
-            // Use the objects
-            Console.WriteLine("The Walmart Employee sample:");
-            Speak(walmartEmployee, "Dan");
+      // Use the objects
+      Console.WriteLine("The Walmart Employee sample:");
+      Speak(walmartEmployee, "Dan");
 
-            Console.WriteLine("The Klingon Warrior sample:");
-            Speak(klingonWarrior, "Worf");
-        }
-
-        static void Speak(Greeter someone, string name)
-        {
-            Console.WriteLine(someone.SayGreeting(name));
-            Console.WriteLine(someone.SayGoodbye());
-            Console.WriteLine(); // blank line
-        }
+      Console.WriteLine("The Klingon Warrior sample:");
+      Speak(klingonWarrior, "Worf");
     }
+
+    // this is a method
+    static void Speak(Greeter someone, string name)
+    {
+      Console.WriteLine(someone.SayGreeting(name));
+      Console.WriteLine(someone.SayGoodbye());
+      Console.WriteLine(); // blank line
+    }
+  }
 }
